7f24bfa5a492a8e3ca986011b5cc093c8dce5f87
Balaji Venkatraman <rslbalaji@gmail.com>
2024-04-15 08:27:55 +0530

    tailwind react code

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscriber from './Subscriber';
+import SuperQuality from './SuperQuality.jsx';
+
+export {
+  Hero,
+  PopularProducts,
+  CustomerReviews,
+  Footer,
+  Services,
+  SpecialOffer,
+  Subscriber,
+  SuperQuality,
+};
